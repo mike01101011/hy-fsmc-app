6be0483eb38bb64e2a2e702cac8669df0a1f7b32
mike01101011 <mike01101011@gmail.com>
2017-12-01 19:42:16 -0500

    Sequential Questions implemented and score calculated

--- client/scripts/components/Quiz.js
+++ client/scripts/components/Quiz.js
@@ -2,19 +2,38 @@
 import React from 'react';
 
 import Question from './Question.js';
+import Score from './Score.js';
 
 class Quiz extends React.Component {
     constructor() { // console.log('quiz-constructor');
         super();
-        this.state = { questions: [] };
+        this.state = { questions: [], question: {}, answer: '', currentQuestionNumber: null, numberOfQuestions: null, score: null };
+        this.handleChange = this.handleChange.bind( this );
+        this.handleSubmit = this.handleSubmit.bind( this );
+        this.fetchQuestions = this.fetchQuestions.bind( this );
+        this.numberOfQuestions = this.numberOfQuestions.bind( this );
+        this.currentQuestion = this.currentQuestion.bind( this );
     }
     render() { console.log('quiz-render-this.state', this.state);
         return (
             <div>
-                <h2>Quiz</h2>
-                { this.state.questions.map( ( question, iteration ) => {
-                    return ( <Question key={ question._id } id={ question._id } question={ question.question } answer={ question.answer } /> );
-                } ) }
+            { this.state.currentQuestionNumber === this.state.numberOfQuestions && this.state.currentQuestionNumber != null ?
+                <Score score={ this.state.score } numberOfQuestions={ this.state.numberOfQuestions } history={ this.props.history } />
+            :
+                <div>
+                    <h2>Quiz</h2>
+                    <p>Question { this.state.currentQuestionNumber + 1 } of { this.state.numberOfQuestions }</p>
+                    <form onSubmit={ this.handleSubmit }>
+                        <input id={ this.props.id } onChange={ this.handleChange } name="answer" type="text" placeholder="Enter answer" value={ this.state.answer } />
+                        <button>Check</button>
+                        { this.state.questions.map( ( question, iteration ) => {
+                            if ( iteration === this.state.currentQuestionNumber ) {
+                                return ( <Question key={ question._id } id={ question._id } question={ question.question } /> );
+                            }
+                        } ) }
+                    </form>
+                </div>
+            }
             </div>
         );
     }
@@ -22,10 +41,23 @@ class Quiz extends React.Component {
         this.fetchQuestions();
     }
     fetchQuestions() { // console.log('quiz-fetchQuestions');
-        fetch( '/api/questions' ).then( resp => resp.json() ).then( json => {
-            this.setState( { questions: json } );
-            // this.setupQuiz( json );
-        } );
+        fetch( '/api/questions' ).then( resp => resp.json() ).then( json => { this.setState( { questions: json } ); } )
+        .then( () => { this.numberOfQuestions(); } );
+    }
+    numberOfQuestions() {
+        this.setState( { numberOfQuestions: this.state.questions.length, currentQuestionNumber: 0 }, () => { this.currentQuestion(); } );
+    }
+    currentQuestion() {
+        this.setState( { question: this.state.questions[ this.state.currentQuestionNumber ] } );
+    }
+    handleChange( e ) { // console.log('question-handleChange');
+        this.setState( { [ e.target.name ]: e.target.value, } );
+    }
+    handleSubmit( e ) { // console.log('question-handleSubmit');
+        e.preventDefault();
+        let score = this.state.score;
+        if ( this.state.answer === this.state.question.answer ) { score += 1; }
+        this.setState( { score: score, answer: '', currentQuestionNumber: (this.state.currentQuestionNumber + 1) }, () => { this.currentQuestion(); } );        
     }
 }
 export default Quiz;
---
+++ client/scripts/components/Score.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Score extends React.Component {
+	constructor() { // console.log('Score-constructor');
+	    super();
+	    this.state = {};
+	    this.handleSubmit = this.handleSubmit.bind( this );
+	}
+	render() { // console.log('Score-render-this.state', this.state);
+		return(
+			<div>
+			    <h2>Score</h2>
+				<p>{ this.props.score } correct out of { this.props.numberOfQuestions }</p>
+			    <form onSubmit={ this.handleSubmit }>
+			        <button>Finish</button>
+			    </form>
+			</div>
+		);
+	}
+	handleSubmit( e ) { // console.log('Score-handleSubmit');
+	    e.preventDefault();
+	    this.props.history.push(`/`);
+	}
+}
+
+export default Score;
